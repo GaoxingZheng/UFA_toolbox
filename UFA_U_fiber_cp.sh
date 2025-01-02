bbcc1c1cadbea70c6ae5dd5230b2c6239d4a6f86
GaoxingZheng <zheng_gao_xing@163.com>
2025-01-02 16:24:26 -0600

    Add files via upload

---
+++ UFA_U_fiber_cp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+### AUTHOR : Gaoxing Zheng
+### Institution: Zhongshan Hospital, Fudan University
+### EMAIL CONTACT: zheng_gao_xing@163.com
+### DATE: 2022-04-14
+
+UFA_U_fiber_cp(){
+
+U_fibers_folder="$1"
+
+# U fiber White matter clustering analysis
+
+cp ${U_fibers_folder}/U_fibers.tck ./WMA_clustering_SWM/U_fibers.tck
+
+tckconvert ./WMA_clustering_SWM/U_fibers.tck ./WMA_clustering_SWM/U_fibers.vtk -force
+
+}
+
+UFA_U_fiber_cp $1
